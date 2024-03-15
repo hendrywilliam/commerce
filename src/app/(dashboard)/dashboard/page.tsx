20039b428ea1ec3da17d6fe059dbb8e85e042015
hendrywilliam <hendriwilliam29@gmail.com>
2024-03-15 11:03:35 +0700

    feat(apps): add more ui in overview

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -4,12 +4,12 @@ import { UserObjectCustomized } from "@/types";
 import { dashboardNavigation } from "@/config/site";
 import { siteStaticMetadata } from "@/config/site";
 import type { Metadata, ResolvingMetadata } from "next";
-import { AddStoreIcon } from "@/components/ui/icons";
+import { AddStoreIcon, PurchaseIcon } from "@/components/ui/icons";
 import { buttonVariants } from "@/components/ui/button";
 import { db } from "@/db/core";
 import { redirect } from "next/navigation";
-import { products } from "@/db/schema";
-import { inArray, sql } from "drizzle-orm";
+import { orders, products } from "@/db/schema";
+import { eq, inArray, sql } from "drizzle-orm";
 
 export async function generateMetadata(
   {},
@@ -29,26 +29,39 @@ export default async function DashboardPage() {
     redirect("/sign-in");
   }
 
-  const userOwnedStoresId = user.privateMetadata.storeId;
+  const userOwnedStoresId = user.privateMetadata.storeId.map((storeId) =>
+    typeof storeId === "string" ? Number(storeId) : storeId,
+  );
 
-  const getAllStoresProduct =
+  // Gyat daym this is so ugly.
+  const [storesProducts, totalOrders, totalPurchase] = await Promise.all([
     userOwnedStoresId.length > 0
       ? await db
           .select({
             count: sql<number>`count('*')`,
           })
           .from(products)
-          .where(
-            inArray(products.storeId, [
-              ...userOwnedStoresId.map((storeId) =>
-                typeof storeId === "string" ? Number(storeId) : storeId,
-              ),
-            ]),
-          )
+          .where(inArray(products.storeId, [...userOwnedStoresId]))
+          .execute()
           .then((result) => result[0].count)
-      : [];
-
-  // const getAllOrders = userOwnedStoresId.length > 0 ? await db.select({count: }).
+      : 0,
+    userOwnedStoresId.length > 0
+      ? await db
+          .select({ count: sql<number>`count('*')` })
+          .from(orders)
+          .where(inArray(orders.storeId, [...userOwnedStoresId]))
+          .execute()
+          .then((result) => result[0].count)
+      : 0,
+    user
+      ? await db
+          .select({ count: sql<number>`count('*')` })
+          .from(orders)
+          .where(eq(orders.userId, user.id))
+          .execute()
+          .then((result) => result[0].count)
+      : 0,
+  ]);
 
   return (
     <div className="h-full">
@@ -57,7 +70,7 @@ export default async function DashboardPage() {
         <p className="text-gray-500">This is what we`ve got for you today.</p>
       </div>
       <div className="grid grid-cols-1 gap-4 *:bg-white lg:grid-cols-4">
-        <div className="flex h-52 w-full flex-col rounded border p-6">
+        <div className="flex h-52 w-full flex-col rounded border p-6 shadow-sm">
           <div className="inline-flex w-full items-center justify-between">
             <p>Stores</p>
             <AddStoreIcon className="h-6 w-6" />
@@ -76,21 +89,53 @@ export default async function DashboardPage() {
             </div>
           </div>
         </div>
-        <div className="flex h-52 w-full flex-col rounded border p-6">
+        <div className="flex h-52 w-full flex-col rounded border p-6 shadow-sm">
           <div className="inline-flex w-full items-center justify-between">
             <p>Products</p>
             <AddStoreIcon className="h-6 w-6" />
           </div>
           <div className="mt-4 flex flex-1 flex-col justify-between">
-            <h1 className="text-3xl font-bold">
-              {user.privateMetadata.storeId.length}
-            </h1>
+            <h1 className="text-3xl font-bold">{storesProducts}</h1>
             <div>
               <Link
-                href="/products"
+                href="/dashboard/stores"
                 className={buttonVariants({ variant: "outline" })}
               >
-                View Stores
+                View Stores Products
+              </Link>
+            </div>
+          </div>
+        </div>
+        <div className="flex h-52 w-full flex-col rounded border p-6 shadow-sm">
+          <div className="inline-flex w-full items-center justify-between">
+            <p>Orders</p>
+            <AddStoreIcon className="h-6 w-6" />
+          </div>
+          <div className="mt-4 flex flex-1 flex-col justify-between">
+            <h1 className="text-3xl font-bold">{totalOrders}</h1>
+            <div>
+              <Link
+                href="/dashboard/stores"
+                className={buttonVariants({ variant: "outline" })}
+              >
+                View Stores Orders
+              </Link>
+            </div>
+          </div>
+        </div>
+        <div className="flex h-52 w-full flex-col rounded border p-6 shadow-sm">
+          <div className="inline-flex w-full items-center justify-between">
+            <p>Purchase</p>
+            <PurchaseIcon className="h-6 w-6" />
+          </div>
+          <div className="mt-4 flex flex-1 flex-col justify-between">
+            <h1 className="text-3xl font-bold">{totalPurchase}</h1>
+            <div>
+              <Link
+                href="/dashboard/purchase"
+                className={buttonVariants({ variant: "outline" })}
+              >
+                View Purchase
               </Link>
             </div>
           </div>
