d49a323f5ed1c99b4d9f5ff17fa8651a1b5d6523
Hendry William <hendriwilliam29@gmail.com>
2025-02-13 21:32:21 +0700

    Add inject secret in workflow

---
+++ backend/scripts/fetch-secrets.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+PROJECT_ID="commerce-450807"
+OUTPUT_FILE="../.env"
+
+# check file
+if [ ! -e $OUTPUT_FILE ]; then  
+    echo ".env file doesnt exist, createing a new one."
+    touch $OUTPUT_FILE
+fi 
+
+> $OUTPUT_FILE
+
+echo "fetch secrets & and inject"
+for SECRET in $(gcloud secrets list --format="value(name)" --project=$PROJECT_ID); do 
+    VALUE=$(gcloud secrets versions access latest --secret=$SECRET --project=$PROJECT_ID)
+    
+    # inject to .env with key=value format. 
+    echo "$SECRET=$VALUE" >> $OUTPUT_FILE
+done
+echo "all set, you are good to go fam."
