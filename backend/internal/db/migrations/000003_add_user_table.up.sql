598fa2a8a2fcbb214234d0c22014c8744ba7c860
Hendry William <hendriwilliam29@gmail.com>
2025-03-08 18:04:34 +0700

    Add default value for fullname column

--- backend/internal/db/migrations/000003_add_user_table.up.sql
+++ backend/internal/db/migrations/000003_add_user_table.up.sql
@@ -2,7 +2,7 @@ CREATE TABLE "users" (
     "id" SERIAL PRIMARY KEY,
     "sub" varchar(50) UNIQUE DEFAULT '',
     "email" varchar(255) UNIQUE NOT NULL,
-    "fullname" varchar(255),
+    "fullname" varchar(255) DEFAULT '',
     "password" varchar(255) DEFAULT '',
     "image_url" text DEFAULT '',
     "authentication_type" varchar(255),
